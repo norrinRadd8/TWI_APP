2ad1fe4a1ee082528d5d70b07e6ae44968c0d131
NorrinRadd <halw@sky.com>
2024-11-23 12:57:36 +0000

    modal adjustments

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,8 @@
-import React from 'react';
-
 const Modal = ({ show, handleClose, children }) => {
-    const showHideClassName = show ? "modal display-block" : "modal display-none";
+    const showHideClassName = show ? "modal show" : "modal";
 
     const handleClickOutside = (event) => {
-        if (event.target.className === 'modal display-block') {
+        if (event.target.className.includes('modal')) {
             handleClose();
         }
     };
@@ -12,13 +10,54 @@ const Modal = ({ show, handleClose, children }) => {
     return (
         <div className={showHideClassName} onClick={handleClickOutside}>
             <section className="modal-main">
-            <button className="close-button" onClick={handleClose}>
-                    &#8617; {/* This is the HTML entity for a left arrow */}
-                </button>
+                
                 {children}
             </section>
+            <button className="close-button" onClick={handleClose}>
+                    &#10005;
+                </button>
         </div>
     );
 };
 
+
+
+// import React from 'react';
+
+// const Modal = ({ show, handleClose, children }) => {
+//     const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+//     const handleClickOutside = (event) => {
+//         if (event.target.className === 'modal display-block') {
+//             handleClose();
+//         }
+//     };
+
+//     return (
+//         <div className={showHideClassName} onClick={handleClickOutside}>
+//           <button className="close-button" onClick={handleClose}>
+//             &#8617;
+//           </button>
+//           <section className="modal-main">
+//             {children}
+//           </section>
+//         </div>
+//       );
+      
+
+
+// };
+
 export default Modal;
+
+
+    // return (
+    //     <div className={showHideClassName} onClick={handleClickOutside}>
+    //         <section className="modal-main">
+    //         <button className="close-button" onClick={handleClose}>
+    //                 &#8617; {/* This is the HTML entity for a left arrow */}
+    //             </button>
+    //             {children}
+    //         </section>
+    //     </div>
+    // );
